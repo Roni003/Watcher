727bf9fe57b8f8ef5c8a064941f66a48ae6296c3
Roni K <61784529+Roni003@users.noreply.github.com>
2025-02-11 21:04:03 +0000

    fix user interface

--- backend/src/interfaces/user-interface.ts
+++ backend/src/interfaces/user-interface.ts
@@ -2,10 +2,7 @@ import { TriggerType } from "./trigger-interface";
 
 export interface IUser {
   id: string;
-  // Decide login method
-  //   email: string;
-  //   password: string;
-  // phone: string;
+  email: string;
   createdAt: Date;
   // For each trigger type, store a list of blacklisted location names
   blacklist: Map<TriggerType, Array<string>>;
