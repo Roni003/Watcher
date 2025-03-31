081846022ac829b0b170561d38f1c6cb9e4cd0a7
Roni K <61784529+Roni003@users.noreply.github.com>
2025-04-01 00:12:34 +0100

    backend: add rate limiting

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import morgan from "morgan";
+import rateLimit from "express-rate-limit";
 
 import config from "./config/config";
 import reminderRoutes from "./routes/reminders";
@@ -8,6 +9,10 @@ import { authMiddleware } from "./middleware/auth";
 
 const app: express.Application = express();
 const port: number = config.port;
+const rateLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
 
 app.listen(port, () => {
   console.log(`Running in mode: ${config.environment}`);
@@ -19,6 +24,7 @@ app.listen(port, () => {
 // Parse JSON bodies, Might need one later for cookies
 app.use(morgan(config.environment === "dev" ? "dev" : "combined"));
 app.use(express.json());
+app.use(rateLimiter);
 
 app.use(authMiddleware);
 
